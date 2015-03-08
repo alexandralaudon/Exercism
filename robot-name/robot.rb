196868152d8d1d5bb0a997404c093272a7bb30f5
Alex <alexandralaudon@gmail.com>
2015-03-08 14:37:25 -0600

    Completed nth-Prime exercise

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -2,14 +2,19 @@ class Robot
   attr_reader :name
 
   def initialize
-    @name = [*('A'..'Z')].sample(2).join + [*(0..9)].sample(3).join.to_s
+    create_name
   end
 
-  def reset()
-    old_name = @name
+  def reset(old_name = @name)
     until @name != old_name
-      @name = [*('A'..'Z')].sample(2).join + [*(0..9)].sample(3).join.to_s
+      create_name
     end
+  end
+
+  private
+
+  def create_name
+    @name = [*('A'..'Z')].sample(2).join + [*(0..9)].sample(3).join.to_s
   end
 
 end
